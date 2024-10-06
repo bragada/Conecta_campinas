164b2361fc3c3ea5161f71471c46f48b9e055eec
Henrique Kanso Bragada <hkbragada@gmail.com>
2024-10-06 17:30:38 -0300

    Update upload_api.R

--- upload_api.R
+++ upload_api.R
@@ -1,18 +1,18 @@
-check_and_install <- function(pkg) {
-  if (!require(pkg, character.only = TRUE)) {
-    install.packages(pkg, dependencies = TRUE)
-    library(pkg, character.only = TRUE)
-  }
-}
+#check_and_install <- function(pkg) {
+#  if (!require(pkg, character.only = TRUE)) {
+#    install.packages(pkg, dependencies = TRUE)
+#    library(pkg, character.only = TRUE)
+#  }
+#}
 
 # Lista de pacotes necessários
-required_packages <- c("httr", "jsonlite", "janitor", "tidyverse", "aws.s3", "arrow")
+#required_packages <- c("httr", "jsonlite", "janitor", "tidyverse", "aws.s3", "arrow")
 
-# Verificação e instalação dos pacotes
-lapply(required_packages, check_and_install)
-
-
-`%!in%` <- Negate(`%in%`) 
+## Verificação e instalação dos pacotes
+#lapply(required_packages, check_and_install)
+library(httr)
+print("parece que foi!")
+#`%!in%` <- Negate(`%in%`) 
 
 
 
