92cda21714b640b2ab2b43f87434cbc1b2874f40
Henrique Kanso Bragada <hkbragada@gmail.com>
2025-01-05 19:25:13 -0300

    Update upload_api.R

--- upload_api.R
+++ upload_api.R
@@ -9,10 +9,7 @@ library(tidyverse)
 library(aws.s3)
 library(arrow)
 
-credenciais <- paste0(Sys.getenv("henrique.k"), ":",
-                      Sys.getenv("HKconecta6282?")) %>%
-      base64_enc() %>% 
-      paste("Basic", .)
+
 
 
 
